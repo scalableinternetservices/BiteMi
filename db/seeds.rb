ec49e8838f0c3b762366e86ed9a621432b70460f
miyueyang <miyueyang@gmail.com>
2015-10-25 23:56:35 -0700

    added seeds for listing photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#### Helper to seed listing photos #####
+#### please put all the product listing photos under images/seed ####
+def listing_photo_seeder (listing, image_path, description)
+  listingphoto = ProductPhoto.create! :listing => listing
+  listingphoto.picture = File.open(Rails.root + image_path)
+  listingphoto.description = description
+  listingphoto.save!
+end
+########
 User.delete_all
 Listing.delete_all
 Order.delete_all
@@ -31,28 +41,39 @@ user5.save!
 
 
 listing11 = Listing.create!( {brand: "UArm", model: "V1", price: "1000.0", condition: "good", start_time: "2015-10-19", end_time: "2015-11-26", description: "my precious!", status: nil, user_id: user1.id})
+  listing_photo_seeder listing11, 'public/images/seed/uarm1.jpg', 'such arm'
+  listing_photo_seeder listing11, 'public/images/seed/uarm2.jpg', 'so cooooool!'
+  listing_photo_seeder listing11, 'public/images/seed/uarm3.jpg', 'wow'
 listing12 = Listing.create!( {brand: "UArm", model: "V12", price: "1000.0", condition: "good", start_time: "2015-10-19", end_time: "2015-11-28", description: "my precious!", status: nil, user_id: user1.id})
- listing13 = Listing.create!({brand: "UArm", model: "V21", price: "2000.0", condition: "good", start_time: "2015-10-19", end_time: "2015-11-19", description: "my precious!", status: nil, user_id: user1.id})
- listing14 = Listing.create!({brand: "UArm", model: "V24", price: "2000.0", condition: "good", start_time: "2015-10-19", end_time: "2015-11-9", description: "my precious!", status: nil, user_id: user1.id})
- listing15 = Listing.create!({brand: "UArm", model: "V28", price: "2000.0", condition: "good", start_time: "2015-10-19", end_time: "2016-2-20", description: "my precious!", status: nil, user_id: user1.id})
- listing16 = Listing.create!({brand: "UArm", model: "V3", price: "1000.0", condition: "good", start_time: "2015-10-19", end_time: "2016-2-20", description: "my precious!", status: nil, user_id: user1.id})
+  listing_photo_seeder listing12, 'public/images/seed/uarm1.jpg', 'such arm'
+  listing_photo_seeder listing12, 'public/images/seed/uarm2.jpg', 'why so coool'
+  listing_photo_seeder listing12, 'public/images/seed/uarm3.jpg', 'everyone should get one'
+listing13 = Listing.create!({brand: "UArm", model: "V21", price: "2000.0", condition: "good", start_time: "2015-10-19", end_time: "2015-11-19", description: "my precious!", status: nil, user_id: user1.id})
+listing14 = Listing.create!({brand: "UArm", model: "V24", price: "2000.0", condition: "good", start_time: "2015-10-19", end_time: "2015-11-9", description: "my precious!", status: nil, user_id: user1.id})
+listing15 = Listing.create!({brand: "UArm", model: "V28", price: "2000.0", condition: "good", start_time: "2015-10-19", end_time: "2016-2-20", description: "my precious!", status: nil, user_id: user1.id})
+listing16 = Listing.create!({brand: "UArm", model: "V3", price: "1000.0", condition: "good", start_time: "2015-10-19", end_time: "2016-2-20", description: "my precious!", status: nil, user_id: user1.id})
 
- listing21 = Listing.create!({brand: "UARm", model: "V2", price: "20.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2016-2-20", description: "don't take it away!", status: nil, user_id: user2.id})
- listing22 = Listing.create!({brand: "Uarm", model: "V200", price: "2.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2016-2-20", description: "don't take it away!", status: nil, user_id: user2.id})
- listing23 = Listing.create!({brand: "Dodge", model: "Challenger", price: "0.1", condition: "New", start_time: "2015-10-19", end_time: "2016-2-20", description: "Much Faster than Passat!", status: nil, user_id: user2.id})
- listing24 = Listing.create!({brand: "Jeep", model: "Renegade", price: "250.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2015-12-25", description: "Much bigger than Smart!", status: nil, user_id: user2.id})
+listing21 = Listing.create!({brand: "UARm", model: "V2", price: "20.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2016-2-20", description: "don't take it away!", status: nil, user_id: user2.id})
+listing22 = Listing.create!({brand: "Uarm", model: "V200", price: "2.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2016-2-20", description: "don't take it away!", status: nil, user_id: user2.id})
+listing23 = Listing.create!({brand: "Dodge", model: "Challenger", price: "0.1", condition: "New", start_time: "2015-10-19", end_time: "2016-2-20", description: "Much Faster than Passat!", status: nil, user_id: user2.id})
+listing24 = Listing.create!({brand: "Jeep", model: "Renegade", price: "250.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2015-12-25", description: "Much bigger than Smart!", status: nil, user_id: user2.id})
 
- listing31 = Listing.create!({brand: "democracy!", model: "the newest one", price: "2.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2016-2-20", description: "yeah!", status: nil, user_id: user3.id})
- listing32 = Listing.create!({brand: "freedom!", model: "the newest one", price: "2.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2016-2-20", description: "", status: nil, user_id: user3.id})
- listing33 = Listing.create!({brand: "freedom!", model: "the older one", price: "2.0", condition: "fairly new", start_time: "2015-10-23", end_time: "2016-2-20", description: "", status: nil, user_id: user3.id})
- listing34 = Listing.create!({brand: "freedom!", model: "the coolest one", price: "2.0", condition: "fairly new", start_time: "2015-11-1", end_time: "2016-2-20", description: "", status: nil, user_id: user3.id})
+listing31 = Listing.create!({brand: "democracy!", model: "the newest one", price: "2.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2016-2-20", description: "yeah!", status: nil, user_id: user3.id})
+listing32 = Listing.create!({brand: "freedom!", model: "the newest one", price: "2.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2016-2-20", description: "", status: nil, user_id: user3.id})
+listing33 = Listing.create!({brand: "freedom!", model: "the older one", price: "2.0", condition: "fairly new", start_time: "2015-10-23", end_time: "2016-2-20", description: "", status: nil, user_id: user3.id})
+listing34 = Listing.create!({brand: "freedom!", model: "the coolest one", price: "2.0", condition: "fairly new", start_time: "2015-11-1", end_time: "2016-2-20", description: "", status: nil, user_id: user3.id})
 
- listing41 = Listing.create!({brand: "Apeture Lab", model: "portal gun", price: "0.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2016-2-20", description: "For science!", status: nil, user_id: user4.id})
- listing42 = Listing.create!({brand: "Apeture Lab", model: "portal gun", price: "50.0", condition: "broken", start_time: "2015-10-19", end_time: "2016-2-20", description: "For science!", status: nil, user_id: user4.id})
- listing43 = Listing.create!({brand: "Apeture Lab", model: "portal gun", price: "20.0", condition: "broken", start_time: "2015-10-19", end_time: "2016-2-20", description: "For science!", status: nil, user_id: user4.id})
+listing41 = Listing.create!({brand: "Apeture Lab", model: "portal gun", price: "0.0", condition: "fairly new", start_time: "2015-10-19", end_time: "2016-2-20", description: "For science!", status: nil, user_id: user4.id})
+  listing_photo_seeder listing41, 'public/images/seed/portal_gun.jpg', 'portal here, portal there'
+listing42 = Listing.create!({brand: "Apeture Lab", model: "portal gun", price: "50.0", condition: "broken", start_time: "2015-10-19", end_time: "2016-2-20", description: "For science!", status: nil, user_id: user4.id})
+  listing_photo_seeder listing42, 'public/images/seed/portal_gun.jpg', 'the cake is a lie'
+listing43 = Listing.create!({brand: "Apeture Lab", model: "portal gun", price: "20.0", condition: "broken", start_time: "2015-10-19", end_time: "2016-2-20", description: "For science!", status: nil, user_id: user4.id})
 
- listing51 = Listing.create!({brand: "Meeseeks box", model: "V0", price: "1000000.0", condition: "broken", start_time: "2015-10-19", end_time: "2016-2-20", description: "woooooooo!", status: nil, user_id: user5.id})
- listing52 = Listing.create!({brand: "Jerry", model: "V0", price: "-1000000.0", condition: "broken", start_time: "2015-10-19", end_time: "2016-2-20", description: "woooooooo!", status: nil, user_id: user5.id})
+listing51 = Listing.create!({brand: "Meeseeks box", model: "V0", price: "1000000.0", condition: "broken", start_time: "2015-10-19", end_time: "2016-2-20", description: "woooooooo!", status: nil, user_id: user5.id})
+  listing_photo_seeder listing51, 'public/images/seed/meeseeks_box1.jpg', 'just press the button'
+  listing_photo_seeder listing51, 'public/images/seed/meeseeks_box2.png', 'I\'m Mr.Meeseeks! Look at me!'
+listing52 = Listing.create!({brand: "Jerry", model: "V0", price: "-1000000.0", condition: "broken", start_time: "2015-10-19", end_time: "2016-2-20", description: "woooooooo!", status: nil, user_id: user5.id})
+  listing_photo_seeder listing52, 'public/images/seed/jerry.jpg', 'Ummmm... where am I?'
 
 Order.create!([
   {user_id: user4.id, listing_id: listing12.id, status: "Processing", start_time: "2015-10-24", end_time: "2015-10-27", price: listing12.price}, 
